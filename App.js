83994228f54022076412a9a426cee208e4ec9fff
corbettg <corbettg@uncw.edu>
2017-07-20 16:51:54 -0400

    Small On Press and State Example

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      backgroundColor: 'white'
+    }
+    this.changeColor = this.changeColor.bind(this)
+  }
+
+  changeColor(backgroundColor) {
+    this.setState({backgroundColor:backgroundColor})
+
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.intro}>Welcome to Rescape!</Text>
+      <View style={[styles.container, {backgroundColor: this.state.backgroundColor}]}>
+        <Text style={styles.package} onPress={() => this.changeColor('green')}>Aliean Ship Escape</Text>
+        <Text style={styles.package} onPress={() => this.changeColor('red')}>Santas Shop Escape</Text>
+        <Text style={styles.package} onPress={() => this.changeColor('blue')}>Frozen Forest Escape</Text>
       </View>
     );
   }
@@ -18,10 +34,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  intro: {
-    backgroundColor: 'red',
-    color: '#000',
-    fontSize: 70,
+  package: {
+    backgroundColor: '#fff',
+    color: 'green',
+    fontSize: 30,
+    borderWidth: 2,
+    borderRadius: 2,
+    padding: 10,
+    margin: 20,
+    // width: Dimensions.get('window').width - 10,    This is the same thing as below
+    alignSelf: 'stretch',
     textAlign: 'center',
   }
 });
