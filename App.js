903396c185bfd247b11d8152e224b54ddc9115c3
Elliot Simpson <Elliotsminion@gmail.com>
2017-07-22 21:21:11 -0400

    Revert "successful api call"
    This reverts commit 820660f9cfb767a4bfd88e3dc5b06e092104136a.

--- App.js
+++ App.js
@@ -2,51 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View, } from 'react-native';
 
 export default class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      users: []
-    }
-  }
-  componentDidMount() {
-    this.fetchData();
-  }
-  fetchData() {
-    fetch('http://192.168.1.132:3000') //My local ip address (so the phone can access it)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          users: responseData,
-        });
-      })
-      .done();
-  }
   render() {
-    if (this.state.users.length == 0) {
-      return this.renderLoadingView();
-    }
-
-    var user = this.state.users[0];
-    return this.renderUser(user);
-
-  }
-
-  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text>Loading users...</Text>
+        <Text>Welcome to Rescape!</Text>
       </View>
     );
-  }
-
-  renderUser(user) {
-    return (
-      <View style={styles.container}>
-        <Text>{user.firstName} {user.lastName}</Text>
-        <Text>{user.age} - {user.netWorth}</Text>
-      </View>
-    );  
   }
 }
 
